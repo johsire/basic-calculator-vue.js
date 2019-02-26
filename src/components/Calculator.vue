e7709137943e0602a90226f4ae964d6c0bf666c4
dev_johsire <josiah5115@gmail.com>
2019-02-25 23:00:50 -0700

    Healper Function- Code Refacotred

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -26,54 +26,54 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       previous: null,
       current: '',
       operator: null,
-      operatorClicked: false,
+      operatorClicked: false
     }
   },
   methods: {
-    clear() {
+    clear () {
       this.current = '';
     },
-    sign() {
+    sign () {
       this.current = this.current.charAt(0) === '-' ?
         this.current.slice(1) : `-${this.current}`;
     },
-    percent() {
+    percent () {
       this.current = `${parseFloat(this.current) / 100}`
     },
-    append(number) {
+    append (number) {
       this.current = `${this.current}${number}`;
     },
-    dot() {
+    dot () {
       if (this.current.indexOf('.') === -1) {
         this.append('.');
       }
     },
-    divide() {
+    setPrevious () {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide () {
       this.operator = (a, b) => a / b;
-      this.previous = current;
-      this.operatorClicked = true;
+      this.setPrevious();
     },
-    times() {
+    times () {
       this.operator = (a, b) => a * b;
-      this.previous = current;
-      this.operatorClicked = true;
+      this.setPrevious();
     },
-    subtract() {
+    subtract () {
       this.operator = (a, b) => a - b;
-      this.previous = current;
-      this.operatorClicked = true;
+      this.setPrevious();
     },
-    add() {
+    add () {
       this.operator = (a, b) => a + b;
-      this.previous = current;
-      this.operatorClicked = true;
+      this.setPrevious();
     },
-    equal() {
+    equal () {
     }
   }
 }
